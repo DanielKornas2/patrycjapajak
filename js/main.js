529379bd9849f1d48e53ecd7cde29210b45d343c
Daniel Kornas <harvi_pl@wp.pl>
2019-01-30 13:18:21 +0100

    Mobile styles, toggleClass on mobiles

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const prevImage = document.getElementById("prev");
 const nextImage = document.getElementById("next");
 const mainImage = document.getElementById("mainImage");
+const hamburgerMenu = document.getElementById("hamburgerMenu");
+const logoLink = document.getElementById("logoLink");
+const infoBox = document.getElementById("infoBox");
 const imagesListHomepage = ["https://picsum.photos/788/461", "https://picsum.photos/789/463", "https://picsum.photos/790/463", "https://picsum.photos/786/461"];
 
 let imageIndex = 0;
@@ -28,8 +31,6 @@ const prevImageChange = (imagesSource) => {
     mainImage.src = imagesSource[imageIndex];
 }
 
-
-
 //swiped-right and swiped-left - e listeners from very light pure-swipe library
 nextImage.addEventListener("click", () => nextImageChange(imagesListHomepage));
 mainImage.addEventListener("swiped-right", () => nextImageChange(imagesListHomepage));
@@ -37,3 +38,9 @@ mainImage.addEventListener("swiped-right", () => nextImageChange(imagesListHomep
 prevImage.addEventListener("click", () => prevImageChange(imagesListHomepage));
 mainImage.addEventListener("swiped-left", prevImageChange(imagesListHomepage));
 
+
+hamburgerMenu.addEventListener("click", () => {
+    infoBox.classList.toggle("show");
+    logoLink.classList.toggle("whiteColor");
+    hamburgerMenu.classList.toggle("whiteColor");
+})
